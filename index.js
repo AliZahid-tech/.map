63735f763ad938687f6e1c2ca55c271b0dc2e91a
AliZahid-tech <alizahidkhan822@gmail.com>
2025-12-16 11:49:56 +0500

    refactor

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 import { playlistArr } from "/playlist.js";
 
-const playlistHtml = [];
-playlistArr.map((obj, ind) => {
-  playlistHtml.push(
-    `<section class="card">
+const playlistHtml = playlistArr.map((obj, ind) => {
+  return `<section class="card">
     <div class="card-start">
-        <img src="/images/${playlistArr[ind].albumArt}">
+        <img src="/images/${obj.albumArt}">
     </div>
         <div class="card-mid">
-            <h4 class="card-title">${playlistArr[ind].title}</h4>
-            <p class="card-artist">${playlistArr[ind].artist}</p>
+            <h4 class="card-title">${obj.title}</h4>
+            <p class="card-artist">${obj.artist}</p>
         </div>
     <div class="card-end">
         <p class="card-menu">...</p>
     </div>
 </section>
-    `
-  );
+    `;
 });
 
 document.getElementById("container").innerHTML = playlistHtml;
